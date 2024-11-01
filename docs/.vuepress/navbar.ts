eb9b2d32b0d71672b70c90560dff62420278e045
SnugglePuff <34593206+sunshang-hl@users.noreply.github.com>
2024-11-01 11:38:25 +0800

    Update navbar.ts 测试

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -3,8 +3,8 @@ import { defineNavbarConfig } from 'vuepress-theme-plume'
 export const zhNavbar = defineNavbarConfig([
   { text: '首页', link: '/' },
   { text: '博客', link: '/blog/' },
-  { text: '标签', link: '/blog/tags/' },
-  { text: '归档', link: '/blog/archives/' },
+  { text: '标签1', link: '/blog/tags/' },
+  { text: '归档1', link: '/blog/archives/' },
   {
     text: '笔记',
     items: [{ text: '示例', link: '/notes/demo/README.md' }]
